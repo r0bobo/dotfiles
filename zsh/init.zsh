1919d7adc7c08f0b7a09b13157d44d4b6d2a9c48
Dean Todevski <dean.todevski@gmail.com>
2017-01-09 22:55:33 +0100

    "Added paths file"

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -5,8 +5,10 @@ export ZSH=~/.oh-my-zsh
 
 # ZSH_THEME="agnoster"
 
+source $DOTFILES_DIR/zsh/paths.zsh
 source $DOTFILES_DIR/zsh/general.zsh
 source $DOTFILES_DIR/zsh/aliases.zsh
 source $DOTFILES_DIR/zsh/commands.zsh
 source $DOTFILES_DIR/zsh/zplug.zsh
+
 source $ZSH/oh-my-zsh.sh
---
+++ zsh/paths.zsh
@@ -0,0 +1,6 @@
+# Setup path variables
+
+export DOTFILES_DIR=~/dotfiles
+export ZSH=~/.oh-my-zsh
+export ZPLUG_HOME=~/.zplug
+export VIMPLUG_HOME=~/.local/share/nvim/site/autoload/plug.vim
