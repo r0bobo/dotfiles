3a62cf142f95986c5d5b1804df3225fe8585bf47
Dean Todevski <dean.todevski@gmail.com>
2017-01-09 13:14:25 +0100

    "Sourcing oh-my-zsh"

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -1,10 +1,12 @@
 # Zsh init script
 
 export DOTFILES_DIR=~/dotfiles
+export ZSH=~/.oh-my-zsh
 
+# ZSH_THEME="agnoster"
 
-source $DOTFILES_DIR/zsh/general.zsh 
-source $DOTFILES_DIR/zsh/aliases.zsh 
-source $DOTFILES_DIR/zsh/commands.zsh 
-source $DOTFILES_DIR/zsh/zplug.zsh 
-
+source $DOTFILES_DIR/zsh/general.zsh
+source $DOTFILES_DIR/zsh/aliases.zsh
+source $DOTFILES_DIR/zsh/commands.zsh
+source $DOTFILES_DIR/zsh/zplug.zsh
+source $ZSH/oh-my-zsh.sh
