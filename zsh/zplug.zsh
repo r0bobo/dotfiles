338385267ab956701b010c3f33f9e9d91130a702
Dean Todevski <dean.todevski@gmail.com>
2017-01-09 22:55:55 +0100

    "Added example themes"

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -1,7 +1,9 @@
 # Zplug plugin setup
 
-export ZPLUG_HOME=~/.zplug
-
-source ~/.zplug/init.zsh
+source $ZPLUG_HOME/init.zsh
 
 zplug "zsh-users/zsh-history-substring-search"
+
+# zplug "agnoster/agnoster-zsh-theme", use:agnoster.zsh-theme
+
+# zplug "bhilburn/powerlevel9k", use:powerlevel9k.zsh-theme
