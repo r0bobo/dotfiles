00212b757cb01ad83c3fd29e1082d9f19408e590
Dean Todevski <dean.todevski@gmail.com>
2017-01-09 08:04:47 +0100

    Added zplug zsh plugin manager

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -2,7 +2,9 @@
 
 export DOTFILES_DIR=~/dotfiles
 
+
 source $DOTFILES_DIR/zsh/general.zsh 
 source $DOTFILES_DIR/zsh/aliases.zsh 
 source $DOTFILES_DIR/zsh/commands.zsh 
+source $DOTFILES_DIR/zsh/zplug.zsh 
 
---
+++ zsh/zplug.zsh
@@ -0,0 +1,7 @@
+# Zplug plugin setup
+
+export ZPLUG_HOME=~/.zplug
+
+source ~/.zplug/init.zsh
+
+zplug "zsh-users/zsh-history-substring-search"
