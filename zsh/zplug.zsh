9d7c1940d0af78323eb5c577286e846971d4a600
Dean Todevski <dean.todevski@gmail.com>
2017-01-10 15:21:13 +0100

    "Updated plugin list"

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -4,6 +4,16 @@ source $ZPLUG_HOME/init.zsh
 
 zplug "zsh-users/zsh-history-substring-search"
 
+zplug "agnoster/agnoster-zsh-theme", use:"agnoster.zsh-theme" 
+
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
+
 # zplug "agnoster/agnoster-zsh-theme", use:agnoster.zsh-theme
 
 # zplug "bhilburn/powerlevel9k", use:powerlevel9k.zsh-theme
+
+# zplug "mafredri/zsh-async", from:github 
+
+# zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
+
+zplug load
