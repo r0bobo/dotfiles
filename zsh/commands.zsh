a9631eadb080ad0dbf9c64c025469fdb0c7796c2
Dean Todevski <dean.todevski@gmail.com>
2017-01-10 18:31:33 +0100

    "Autostart tmux if exists and not running"

--- zsh/commands.zsh
+++ zsh/commands.zsh
@@ -4,6 +4,10 @@ if [[ $TERMINIX_ID ]]; then
     source /etc/profile.d/vte.sh
 fi
 
+if command -v tmux>/dev/null; then
+  [[ ! $TERM =~ screen ]] && [ -z $TMUX ] && exec tmux
+fi
+
 if hash neofetch 2>/dev/null;then
    neofetch
 fi
