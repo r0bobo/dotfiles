fd281b216a5ea789851f8954106e6258485e0841
Dean Todevski <dean.todevski@gmail.com>
2017-01-15 21:31:39 +0100

    Added default paths

---
+++ zsh/paths.sh
@@ -0,0 +1,8 @@
+#!/bin/env bash
+# Setup path variables
+
+export DOTFILES_DIR=${DOTFILES_DIR-~/dotfiles}
+export ZSH=${ZSH-~/.oh-my-zsh}
+export ZPLUG_HOME=${ZPLUG_HOME-~/.zplug}
+export VIMPLUG_HOME=${VIMPLUG_HOME-~/.local/share/nvim/site/autoload/plug.vim}
+
