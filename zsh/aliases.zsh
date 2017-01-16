f34a4b7813b4ac40c1b0c755a7e711e58eb76c49
r0bobo <dean.todevski@gmail.com>
2017-01-16 14:33:24 +0100

    Update aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,6 +5,6 @@ alias zshreload="source ~/.zshrc"
 if hash termux-fix-shebang 2>/dev/null;then
     alias upgrade_dotfiles="sh $DOTFILES_DIR/scripts/setup-dotfiles-termux" 
 else 
-    alias upgrade_dotfiles="sh $DOTFILES_DIR/scripts/setup-dotfiles" 
+    alias upgrade_dotfiles="zsh $DOTFILES_DIR/scripts/setup-dotfiles" 
 fi
 
