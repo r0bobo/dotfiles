43b64fd9e027e0d11089f37f440138c0bc15cf5a
r0bobo <dean.todevski@gmail.com>
2017-01-16 08:56:29 +0100

    Removed travis debug print()

--- scripts/create_symlinks.py
+++ scripts/create_symlinks.py
@@ -38,8 +38,6 @@ class dotfiles:
         for link in self.link_list:
             if os.path.exists(link[0]):
                 try:
-                    print(link[0])
-                    print(link[1])
                     os.symlink(link[0], link[1])
                 except FileExistsError:
                     if os.path.islink(link[1]):
