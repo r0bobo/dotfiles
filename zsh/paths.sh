4a49eb246b1ca20afbaf2581f701ce8a64a58b11
r0bobo <dean.todevski@gmail.com>
2017-01-16 14:15:09 +0100

    Corrected syntax

--- zsh/paths.sh
+++ zsh/paths.sh
@@ -1,8 +1,7 @@
 #!/bin/env bash
 # Setup path variables
 
-export DOTFILES_DIR=${DOTFILES_DIR-~/dotfiles}
-export ZSH=${ZSH-~/.oh-my-zsh}
-export ZPLUG_HOME=${ZPLUG_HOME-~/.zplug}
-export VIMPLUG_HOME=${VIMPLUG_HOME-~/.local/share/nvim/site/autoload/plug.vim}
-
+: ${DOTFILES_DIR:=~/dotfiles}
+: ${ZSH:=~/.oh-my-zsh}
+: ${ZPLUG_HOME:=~/.zplug}
+: ${VIMPLUG_HOME:=~/.local/share/nvim/site/autoload/plug.vim}
