0ce43b55d072aec0b8e5f6d00bafb11b13c3a5b6
r0bobo <dean.todevski@gmail.com>
2017-01-17 12:40:48 +0100

    Add root indicator

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,6 +1,6 @@
 # Powerlevel9k Settings
 
-export POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs)
+export POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(root_indicator context dir vcs)
 export POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status virtualenv time)
 export POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
 export POWERLEVEL9K_SHORTEN_DELIMITER=""
