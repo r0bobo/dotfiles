4fe66c975f9de87ac9ce6e13814c2a0e57045622
Dean Todevski <dean.todevski@gmail.com>
2017-01-17 07:17:58 +0100

    Remove substring-search

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -2,11 +2,11 @@
 
 source $ZPLUG_HOME/init.zsh
 
-zplug "zsh-users/zsh-history-substring-search"
-
-zplug "zsh-users/zsh-syntax-highlighting", defer:2
+# zplug "zsh-users/zsh-history-substring-search"
 
 zplug "zsh-users/zsh-autosuggestions"
+
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
 
 # Themes
