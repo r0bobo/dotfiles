6555fafe619d67a035186daa677abf83b0d82bbf
r0bobo <dean.todevski@gmail.com>
2017-01-18 14:25:18 +0100

    Update history options

--- zsh/general.zsh
+++ zsh/general.zsh
@@ -3,9 +3,22 @@
 export EDITOR='nvim'
 export DEFAULT_USER=`whoami`
 export USER=`whoami`
+
+# History
 export HISTFILE=~/.zsh_history
-export HISTSIZE=100
-export SAVEHIST=100
+export HISTSIZE=1000
+export SAVEHIST=1000
+
+setopt append_history
+setopt hist_ignore_dups
+setopt hist_expire_dups_first
+setopt hist_ignore_space
+setopt hist_verify
+setopt inc_append_history
+setopt share_history
+
+
+
 
 autoload -Uz compinit promptinit run-help
 compinit
@@ -13,10 +26,12 @@ promptinit
 
 setopt correct
 setopt correctall
-setopt AUTO_CD
-setopt append_history share_history histignorealldups
+setopt auto_cd
+setopt auto_pushd
+
 
 zstyle ':completion:*' menu select
 zstyle ':completion:*:default'         list-colors ${(s.:.)LS_COLORS}
 
+# Fix right prompt offset
 ZLE_RPROMPT_INDENT=0
