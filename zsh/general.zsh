1a214e59eec6a27cf3f45b45df0ae4c24295334d
Dean Todevski <dean.todevski@gmail.com>
2017-01-18 07:31:06 +0100

    Add history variables

--- zsh/general.zsh
+++ zsh/general.zsh
@@ -3,6 +3,9 @@
 export EDITOR='nvim'
 export DEFAULT_USER=`whoami`
 export USER=`whoami`
+export HISTFILE=~/.zsh_history
+export HISTSIZE=100
+export SAVEHIST=100
 
 autoload -Uz compinit promptinit run-help
 compinit
