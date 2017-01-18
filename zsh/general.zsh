03245faeacd30915b32b8aaee7c8634bcd6e8401
r0bobo <dean.todevski@gmail.com>
2017-01-18 15:13:33 +0100

    Update general.zsh

--- zsh/general.zsh
+++ zsh/general.zsh
@@ -4,6 +4,7 @@ export EDITOR='nvim'
 export DEFAULT_USER=`whoami`
 export USER=`whoami`
 
+
 # History
 export HISTFILE=~/.zsh_history
 export HISTSIZE=1000
@@ -18,20 +19,27 @@ setopt inc_append_history
 setopt share_history
 
 
+# Completion
+setopt always_to_end
+setopt auto_menu
+setopt complete_in_word
+
+zstyle ':completion:*' menu select
+zstyle ':completion:*:default'  list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion::complete:*' use-cache 1
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
 
 
 autoload -Uz compinit promptinit run-help
 compinit
 promptinit
 
+
 setopt correct
 setopt correctall
 setopt auto_cd
 setopt auto_pushd
 
-
-zstyle ':completion:*' menu select
-zstyle ':completion:*:default'         list-colors ${(s.:.)LS_COLORS}
 
 # Fix right prompt offset
 ZLE_RPROMPT_INDENT=0
