186c956fdbe6787547e11d8e6c884dd25ad4f660
Dean Todevski <dean.todevski@gmail.com>
2017-01-28 00:22:42 +0100

    Fix keybind issues

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -6,9 +6,8 @@ source $DOTFILES_DIR/zsh/paths.sh
 
 # Source config files
 source $DOTFILES_DIR/zsh/general.zsh
+source $DOTFILES_DIR/zsh/keybind.zsh
 source $DOTFILES_DIR/zsh/aliases.zsh
 source $DOTFILES_DIR/zsh/commands.zsh
 source $DOTFILES_DIR/zsh/zplug.zsh
 source $DOTFILES_DIR/zsh/prompt.zsh
-
-# source $ZSH/oh-my-zsh.sh
