9fa6d9fe2ba26970fa78e90152c10801ef06cfed
Dean Todevski <dean.todevski@gmail.com>
2017-02-04 23:56:51 +0100

    Add color to ls alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,8 +4,9 @@ alias zshreload="source ~/.zshrc"
 alias promptreload="source $DOTFILES_DIR/zsh/prompt.zsh"
 
 # Directory
-alias ll='ls -lh'
-alias la='ls -lAh'
+alias ls='ls --color=auto'
+alias ll='ls -lh --color=auto'
+alias la='ls -lAh --color=auto'
 
 if hash termux-fix-shebang 2>/dev/null;then
     alias upgrade_dotfiles="sh $DOTFILES_DIR/scripts/setup-dotfiles-termux" 
