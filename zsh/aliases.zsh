620a450aff848277ed5f0375d4ea56a5fd0856c6
Dean Todevski <dean.todevski@gmail.com>
2017-04-08 16:03:31 +0200

    Remove aliases with errors

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,15 +9,9 @@ alias ll='ls -lh --color=auto'
 alias la='ls -lAh --color=auto'
 
 if hash termux-fix-shebang 2>/dev/null;then
-    alias upgrade_dotfiles="sh $DOTFILES_DIR/scripts/setup-dotfiles-termux" 
-else 
-    alias upgrade_dotfiles="zsh $DOTFILES_DIR/scripts/setup-dotfiles" 
+    alias upgrade_dotfiles="sh $DOTFILES_DIR/scripts/setup-dotfiles-termux"
+else
+    alias upgrade_dotfiles="zsh $DOTFILES_DIR/scripts/setup-dotfiles"
 fi
 
-alias diff='diff --color=auto'  # Color diff
-
 alias grep='grep --color=auto'  # Color grep
-
-# Color manpages
-export MANPAGER='lynx -force_html'
-alias man='man -Thtml'
