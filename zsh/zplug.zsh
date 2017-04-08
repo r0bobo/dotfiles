d05efc6526d5220b44d592a0f19e0b1d1e76a4fe
Dean Todevski <dean.todevski@gmail.com>
2017-04-08 16:06:58 +0200

    Remove plugins

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -2,21 +2,14 @@
 
 source $ZPLUG_HOME/init.zsh
 
-zplug "zsh-users/zsh-history-substring-search"
-
 zplug "zsh-users/zsh-autosuggestions"
-
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
 
 # Themes
 
-# zplug "agnoster/agnoster-zsh-theme", use:"agnoster.zsh-theme"
-
 zplug "bhilburn/powerlevel9k", use:powerlevel9k.zsh-theme
-
 # zplug "mafredri/zsh-async", from:github 
-
 # zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
 
 zplug load
