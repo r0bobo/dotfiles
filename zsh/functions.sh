83e6b3427175e4d5db125255f29841608a9e4612
Dean Todevski <dean.todevski@gmail.com>
2017-05-05 19:22:50 +0200

    Add color to less

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-rmd () {
-  pandoc $1 | lynx -stdin
+man() {
+    LESS_TERMCAP_md=$'\e[01;31m' \
+    LESS_TERMCAP_me=$'\e[0m' \
+    LESS_TERMCAP_se=$'\e[0m' \
+    LESS_TERMCAP_so=$'\e[01;44;33m' \
+    LESS_TERMCAP_ue=$'\e[0m' \
+    LESS_TERMCAP_us=$'\e[01;32m' \
+    command man "$@"
 }
