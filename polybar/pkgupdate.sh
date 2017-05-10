0d03e5c5b32c0e7df5d7278c45d39d32caf1d22f
Dean Todevski <dean.todevski@gmail.com>
2017-05-10 20:53:35 +0200

    Remove extra space

--- polybar/pkgupdate.sh
+++ polybar/pkgupdate.sh
@@ -8,7 +8,7 @@ pkgs=$((pac + aur))
 if [[ "$pkgs" != "0" ]]
 then
     # echo "%{colors.foreground-right}%{T3}%{T-}%{F-}  $pkgs"
-    echo "%{F#ebdbb2}%{T3}%{T-}%{F-}  $pkgs"
+    echo "%{F#ebdbb2}%{T3}%{T-}%{F-} $pkgs"
 else
     # echo "%{colors.foreground-right-inactive}%{T-}%{F-}"
     echo "%{F#928374}%{T-}%{F-}"
