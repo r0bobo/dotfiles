9c7f636d22a08d8f8b8cffdcbea8973916f0fcf9
Dean Todevski <dean.todevski@gmail.com>
2017-05-22 17:43:45 +0200

    Change prompt theme

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -4,12 +4,14 @@ source $ZPLUG_HOME/init.zsh
 
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zplug "changyuheng/zsh-interactive-cd"
 
 
 # Themes
 
-zplug "bhilburn/powerlevel9k", use:powerlevel9k.zsh-theme
-# zplug "mafredri/zsh-async", from:github 
+zplug "mafredri/zsh-async", from:github
+# zplug "bhilburn/powerlevel9k", use:powerlevel9k.zsh-theme
 # zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
+zplug "dfurnes/purer"
 
 zplug load
