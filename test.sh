60b3f999a719f3de5b9e1e3dadf13c71283557f9
Dean Todevski <dean.todevski@gmail.com>
2017-09-04 23:09:31 +0200

    Add more unit tests

--- test.sh
+++ test.sh
@@ -20,6 +20,17 @@ teardown() {
     [ -f testdir/.ctags.~1~ ]
 }
 
+@test "Setup vimplug" {
+    INSTALL_DIR=testdir make vimplug
+    [ -f testdir/.local/share/nvim/site/autoload/plug.vim ]
+}
+
+@test "Setup zplug" {
+    INSTALL_DIR=testdir make zplug
+    run zsh -c "source testdir/.zplug/init.zsh"
+    [ "$status" -eq 0 ]
+}
+
 @test "Setup virtual environment" {
     VENV=testdir/.local/share/nvim/.virtualenv
     INSTALL_DIR=testdir make virtual_env
