eae9f3a16c5a593a41836f779200fe612ade2af0
Dean Todevski <dean.todevski@gmail.com>
2017-10-12 15:30:39 +0200

    Add private mercurial layer

--- configs/.spacemacs.d/init.el
+++ configs/.spacemacs.d/init.el
@@ -26,7 +26,8 @@ This function should only modify configuration layer settings."
    ;; If non-nil layers with lazy install support are lazy installed.
    ;; List of additional paths where to look for configuration layers.
    ;; Paths must have a trailing slash (i.e. `~/.mycontribs/')
-   dotspacemacs-configuration-layer-path '()
+   dotspacemacs-configuration-layer-path
+   '("~/.spacemacs.d/layers/")
    )
 
   ;; Setup different layers for different computers
