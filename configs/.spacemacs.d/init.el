8136bd89635ac8e1e13f618ee4c7da40e15296f2
Dean Todevski <dean.todevski@gmail.com>
2017-11-22 17:33:36 +0100

    Add monokai theme, php and ruby layer

--- configs/.spacemacs.d/init.el
+++ configs/.spacemacs.d/init.el
@@ -65,6 +65,8 @@ This function should only modify configuration layer settings."
      html
      markdown
      nginx
+     php
+     ruby
      rust
      systemd
      version-control
@@ -167,6 +169,7 @@ It should only modify the values of Spacemacs settings."
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(gruvbox-dark-soft
+                         monokai
                          solarized-light)
    ;; If non-nil the cursor color matches the state color in GUI Emacs.
    ;; (default t)
