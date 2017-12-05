a4f52de566eef2e02f6e8260af1f8124d4b7c3ba
Dean Todevski <dean.todevski@gmail.com>
2017-12-05 19:55:24 +0100

    Add doom theme

--- configs/.spacemacs.d/init.el
+++ configs/.spacemacs.d/init.el
@@ -93,7 +93,8 @@ This function should only modify configuration layer settings."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(org-cliplink)
+   dotspacemacs-additional-packages '(org-cliplink
+                                      doom-themes)
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
