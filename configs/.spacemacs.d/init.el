070f8682a9bbcf08988ec2e6e41cc9fbf51b5559
Dean Todevski <dean.todevski@gmail.com>
2017-12-11 22:04:46 +0100

    Add apache-mode

--- configs/.spacemacs.d/init.el
+++ configs/.spacemacs.d/init.el
@@ -94,8 +94,9 @@ This function should only modify configuration layer settings."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(org-cliplink
-                                      doom-themes)
+   dotspacemacs-additional-packages '(apache-mode
+                                      doom-themes
+                                      org-cliplink)
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
