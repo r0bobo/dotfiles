eaee42de5b2384660e56dff369bcbf01fb862a8e
Dean Todevski <dean.todevski@gmail.com>
2017-12-13 21:17:14 +0100

    Create private config layer

---
+++ configs/.spacemacs.d/layers/private-home/layers.el
@@ -0,0 +1,26 @@
+;;; layers.el --- private layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Dean Todevski <dean.todevski@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(configuration-layer/declare-layers
+ '(common-lisp
+   deft
+   django
+   docker
+   github
+   html
+   markdown
+   nginx
+   php
+   ruby
+   rust
+   systemd
+   version-control
+   vimscript))
