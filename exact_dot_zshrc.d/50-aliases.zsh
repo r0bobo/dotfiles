d712cc234474569253194678a170476c23a27fb7
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2020-05-03 22:44:03 +0200

    Add proj alias

--- exact_dot_zshrc.d/50-aliases.zsh
+++ exact_dot_zshrc.d/50-aliases.zsh
@@ -6,3 +6,11 @@ alias less="less -FSRX"
 alias ssh-fix="chmod 600 ~/.ssh/{*/*.key,config}"
 alias virsh-local="virsh -c qemu:///system"
 alias zshreload="source $HOME/.zshrc"
+
+
+proj() {
+    BASE="$HOME/projects"
+    PROJ="$(fd --max-depth=2 --base-directory="$BASE" --type=directory \
+               | fzf --preview='ls $HOME/projects/{}')"
+    cd "$BASE/$PROJ"
+}
