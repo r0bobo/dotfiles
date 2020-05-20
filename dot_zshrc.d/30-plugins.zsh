3b3d344f563867a7d5acf4dd90a7a621faa00bce
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2020-05-20 23:57:51 +0200

    Update zsh plugin paths

--- dot_zshrc.d/30-plugins.zsh
+++ dot_zshrc.d/30-plugins.zsh
@@ -1,6 +1,5 @@
 plugins=(
-    '/usr/share/zsh/plugins/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh'
-    '/usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh'
+    '/usr/share/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh'
     '/usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh'
     '/usr/share/fzf/shell/key-bindings.zsh'
 )
