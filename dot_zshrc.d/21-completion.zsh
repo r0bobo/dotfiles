e8dfbd3f0f6b338de3eedc5be977a564a79ea1ab
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2020-05-25 22:55:19 +0200

    Improve completion setup and startup speed

--- dot_zshrc.d/21-completion.zsh
+++ dot_zshrc.d/21-completion.zsh
@@ -1,38 +1,47 @@
 #!/bin/zsh
 
+zmodload zsh/stat
 
-autoload -Uz compinit
-compinit
-
-autoload -U +X bashcompinit
-bashcompinit
+NOW="$(date +%j)"
+COMPDIR="$HOME/.local/share/zsh/completions"
 
 _bashcomp() {
     cmd=$1
     cmp=$2
-    cmp=${cmp:-$cmd}  # Set to $cmd if undefined
+    cmp="${cmp:-"$cmd"}"  # Set to $cmd if undefined
 
     if command -v $cmd &>/dev/null; then
         complete -o nospace -C $cmp $cmd
     fi
 }
 
-
 _gocomp() {
     cmd=$1
-    if command -v $cmd &>/dev/null; then
-        source <($cmd completion zsh)
+
+    cmpdir="$COMPDIR/_$cmd"
+
+    if [[ ! -r "$cmpdir" ]] || [[ "$NOW" != $(zstat -F '%j' +mtime -- "$cmpdir") ]]; then
+        "$cmd" completion zsh > "$COMPDIR/_$cmd"
     fi
 }
 
+
+mkdir -p "$COMPDIR"
+fpath+="$COMPDIR"
+
+autoload -Uz compinit
+autoload -U +X bashcompinit
+
+
+# Zsh completion for typical go binaries
+_gocomp kubectl
+_gocomp tkn
+
+compinit
+bashcompinit
 
 # Bash completion
 _bashcomp aws aws_completer
 _bashcomp consul
 _bashcomp terraform
 _bashcomp vault
-
-
-# Zsh completion for typical go binaries
-_gocomp kubectl
-_gocomp tkn
