e88d967e6c24982b99592156e4d28ac3b5c1b2c2
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2020-08-16 23:13:21 +0200

    Enable pretty org-mode

--- doom/init.el
+++ doom/init.el
@@ -144,7 +144,8 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +pandoc +present)  ; organize your plain life in plain text
+       (org                ; organize your plain life in plain text
+        +pandoc +present +pretty)
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
