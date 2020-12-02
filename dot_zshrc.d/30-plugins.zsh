322e3bd40cedddf7bfb7a3a420fc62e6be4d546d
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2020-12-02 23:17:16 +0100

    Remove double nix sourcing

--- dot_zshrc.d/30-plugins.zsh
+++ dot_zshrc.d/30-plugins.zsh
@@ -3,7 +3,6 @@
 -plugin /usr/share/fzf/shell/key-bindings.zsh
 -plugin /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 -plugin /usr/share/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
--plugin "$HOME/.nix-profile/etc/profile.d/nix.sh"
 
 eval "$(direnv hook zsh)"
 eval "$(starship init zsh --print-full-init)"
