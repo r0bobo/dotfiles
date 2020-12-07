76d5505da57d9da2aa3728554f5771fddf57ee7b
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2020-12-07 22:24:21 +0100

    Add nixfmt + autoformat

--- dot_config/nixpkgs/config.nix
+++ dot_config/nixpkgs/config.nix
@@ -1,44 +1,40 @@
 {
-  packageOverrides = pkgs: with pkgs; {
-    myPackages = pkgs.buildEnv {
-      name = "my-packages";
-      paths = [
+  packageOverrides = pkgs:
+    with pkgs; {
+      myPackages = pkgs.buildEnv {
+        name = "my-packages";
+        paths = [
 
-        argocd
-        cfssl
-        chezmoi
-        dive
-        dogdns
-        hey
-        go-jsonnet
-        k9s
-        kail
-        kind
-        kpt
-        kubernetes-helm
-        kustomize
-        operator-sdk
-        skaffold
-        starship
-        wrk
-        yq-go
+          argocd
+          cfssl
+          chezmoi
+          dive
+          dogdns
+          go-jsonnet
+          hey
+          k9s
+          kail
+          kind
+          kpt
+          kubernetes-helm
+          kustomize
+          nixfmt
+          operator-sdk
+          skaffold
+          starship
+          wrk
+          yq-go
 
-        # Zsh stuf
-        antibody
-        nix-zsh-completions
-        zsh-autosuggestions
-        zsh-completions
-        zsh-fast-syntax-highlighting
+          # Zsh stuf
+          antibody
+          nix-zsh-completions
+          zsh-autosuggestions
+          zsh-completions
+          zsh-fast-syntax-highlighting
 
-      ];
-      pathsToLink = [
-        "/bin"
-        "/share"
-      ];
-      extraOutputsToInstall = [
-        "man"
-        "doc"
-      ];
+        ];
+        pathsToLink = [ "/bin" "/share" ];
+        extraOutputsToInstall = [ "man" "doc" ];
+      };
     };
-  };
 }
