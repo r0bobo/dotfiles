e96d995a9c156a60402e08aca5ab6c4e814bba5f
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2020-12-18 22:34:10 +0100

    Add org-mode line autoformat

--- doom/config.el
+++ doom/config.el
@@ -174,11 +174,51 @@ appropriate.  In tables, insert a new row or end the table."
        (t
         ;; All other cases: call `org-return'.
         (org-return))))))
-
 (map!
  :after evil-org
  :map evil-org-mode-map
  :i [return] #'unpackaged/org-return-dwim)
+;;;###autoload
+(defun unpackaged/org-fix-blank-lines (&optional prefix)
+  "Ensure that blank lines exist between headings and between headings and their contents.
+With prefix, operate on whole buffer. Ensures that blank lines
+exist after each headings's drawers."
+  (interactive "P")
+  (org-map-entries (lambda ()
+                     (org-with-wide-buffer
+                      ;; `org-map-entries' narrows the buffer, which prevents us from seeing
+                      ;; newlines before the current heading, so we do this part widened.
+                      (while (not (looking-back "\n\n" nil))
+                        ;; Insert blank lines before heading.
+                        (insert "\n")))
+                     (let ((end (org-entry-end-position)))
+                       ;; Insert blank lines before entry content
+                       (forward-line)
+                       (while (and (org-at-planning-p)
+                                   (< (point) (point-max)))
+                         ;; Skip planning lines
+                         (forward-line))
+                       (while (re-search-forward org-drawer-regexp end t)
+                         ;; Skip drawers. You might think that `org-at-drawer-p' would suffice, but
+                         ;; for some reason it doesn't work correctly when operating on hidden text.
+                         ;; This works, taken from `org-agenda-get-some-entry-text'.
+                         (re-search-forward "^[ \t]*:END:.*\n?" end t)
+                         (goto-char (match-end 0)))
+                       (unless (or (= (point) (point-max))
+                                   (org-at-heading-p)
+                                   (looking-at-p "\n"))
+                         (insert "\n"))))
+                   t (if prefix
+                         nil
+                       'tree)))
+(add-hook 'before-save-hook (lambda ()
+                              (if (and
+                                   (eq major-mode 'org-mode) ; Org-mode
+                                   (not (string-equal default-directory (expand-file-name kb/agenda-dir))) ; Not agenda-dir
+                                   (not (string-equal buffer-file-name (expand-file-name "seedbox.org" org-roam-directory))))
+                                  (let ((current-prefix-arg 4)) ; Emulate C-u
+                                    (call-interactively 'unpackaged/org-fix-blank-lines)))
+                              ))
 (after! ansible-doc
   (set-evil-initial-state! '(ansible-doc-module-mode) 'normal))
 
