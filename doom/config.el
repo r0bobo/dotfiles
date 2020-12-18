56238e52b447c13bc42e9444a7ec404d8884d02b
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2020-12-18 23:31:51 +0100

    Add magit smerge hydra from unpackaged

--- doom/config.el
+++ doom/config.el
@@ -209,7 +209,7 @@ exist after each headings's drawers."
                                    (looking-at-p "\n"))
                          (insert "\n"))))
                    t))
-(add-hook 'before-save-hook #'unpackaged/org-fix-blank-lines)
+(add-hook! 'before-save-hook #'unpackaged/org-fix-blank-lines)
 (after! ansible-doc
   (set-evil-initial-state! '(ansible-doc-module-mode) 'normal))
 
@@ -256,6 +256,44 @@ exist after each headings's drawers."
 (use-package! jq-mode
   :mode ("\\.jq" . jq-mode))
 (setq +ivy-buffer-preview t)
+(use-package! smerge-mode
+  :config
+  (defhydra unpackaged/smerge-hydra
+    (:color pink :hint nil :post (smerge-auto-leave))
+    "
+^Move^       ^Keep^               ^Diff^                 ^Other^
+^^-----------^^-------------------^^---------------------^^-------
+_n_ext       _b_ase               _<_: upper/base        _C_ombine
+_p_rev       _u_pper              _=_: upper/lower       _r_esolve
+^^           _l_ower              _>_: base/lower        _k_ill current
+^^           _a_ll                _R_efine
+^^           _RET_: current       _E_diff
+"
+    ("n" smerge-next)
+    ("p" smerge-prev)
+    ("b" smerge-keep-base)
+    ("u" smerge-keep-upper)
+    ("l" smerge-keep-lower)
+    ("a" smerge-keep-all)
+    ("RET" smerge-keep-current)
+    ("\C-m" smerge-keep-current)
+    ("<" smerge-diff-base-upper)
+    ("=" smerge-diff-upper-lower)
+    (">" smerge-diff-base-lower)
+    ("R" smerge-refine)
+    ("E" smerge-ediff)
+    ("C" smerge-combine-with-next)
+    ("r" smerge-resolve)
+    ("k" smerge-kill-current)
+    ("ZZ" (lambda ()
+            (interactive)
+            (save-buffer)
+            (bury-buffer))
+     "Save and bury buffer" :color blue)
+    ("q" nil "cancel" :color blue))
+  :hook (magit-diff-visit-file . (lambda ()
+                                   (when smerge-mode
+                                     (unpackaged/smerge-hydra/body)))))
 (defun dean/doom-config (&optional initial-input)
   "Search Doom private config and jump to a heading."
   (interactive)
