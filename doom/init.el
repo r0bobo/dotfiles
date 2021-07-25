636140224aa179f879b61b4c761dc5e0fe6f5f2b
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2021-07-25 14:43:44 +0200

    Switch to vertico in doom

--- doom/init.el
+++ doom/init.el
@@ -26,8 +26,8 @@
        (company +tng)      ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy +prescient)    ; a search engine for love and life
-       ;;vertico           ; the search engine of the future
+       ;; (ivy +prescient)    ; a search engine for love and life
+       vertico           ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
