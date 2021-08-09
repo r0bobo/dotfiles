bd0974cca4e2f2a7cf5d6c297ab587f76f893a52
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2021-08-09 23:09:11 +0200

    Add prefix based zsh history search with ctrl+p/n

--- .zshrc.d/60-keybinds.zsh
+++ .zshrc.d/60-keybinds.zsh
@@ -6,3 +6,11 @@ bindkey -e # emacs key bindings
 
 bindkey -M menuselect '^j' expand-or-complete
 bindkey -M menuselect '^k' reverse-menu-complete
+
+# Do prefix based history search
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^P" up-line-or-beginning-search # Up
+bindkey "^N" down-line-or-beginning-search # Down
