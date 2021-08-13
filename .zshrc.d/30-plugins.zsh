46edbdb1aaeb4bfc9fc76f7779c9d13db768ee01
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2021-08-13 22:05:06 +0200

    Improve robustness when missing plugins

--- .zshrc.d/30-plugins.zsh
+++ .zshrc.d/30-plugins.zsh
@@ -1,8 +1,9 @@
 #!/usr/bin/env zsh
 
 -plugin /usr/share/fzf/shell/key-bindings.zsh
-source "$HOME/.nix-profile/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
-source "$HOME/.nix-profile/share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh"
+
+-plugin "$HOME/.nix-profile/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+-plugin "$HOME/.nix-profile/share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh"
 
 eval "$(direnv hook zsh)"
 eval "$(starship init zsh --print-full-init)"
