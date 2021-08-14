d9c490b2e4e4c904858026bd155b57dbaa968c09
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2021-08-14 21:15:34 +0200

    Remove old stuff

--- .config/yadm/bootstrap.d/40-remove-files.sh
+++ .config/yadm/bootstrap.d/40-remove-files.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 to_remove=(
     "$HOME/.zshenv"
     "$HOME/.zshrc.d"
+    "$HOME/.local/bin/fman"
+    "$HOME/.local/bin/firefox-link-userjs"
 )
 
 xargs rm -rfv <<<"${to_remove[@]}"
