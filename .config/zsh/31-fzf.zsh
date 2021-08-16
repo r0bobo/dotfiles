4f987ab2cea4d34146c15dbfce9bf6a25a837761
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2021-08-16 18:39:41 +0200

    Improve fzf ctrl+r display

--- .config/zsh/31-fzf.zsh
+++ .config/zsh/31-fzf.zsh
@@ -7,17 +7,20 @@ export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
 --color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7
 '
 
+# I don't want to display the history number
+export FZF_CTRL_R_OPTS=--with-nth=2..
+
 # Use fd (https://github.com/sharkdp/fd) instead of the default find
 # command for listing path candidates.
 # - The first argument to the function ($1) is the base path to start traversal
 # - See the source code (completion.{bash,zsh}) for the details.
 _fzf_compgen_path() {
-	fd --hidden --follow --exclude ".git" . "$1"
+  fd --hidden --follow --exclude ".git" . "$1"
 }
 
 # Use fd to generate the list for directory completion
 _fzf_compgen_dir() {
-	fd --type d --hidden --follow --exclude ".git" . "$1"
+  fd --type d --hidden --follow --exclude ".git" . "$1"
 }
 
 # https://github.com/junegunn/fzf/wiki/Examples-(completion)#zsh-pass
