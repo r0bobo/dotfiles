4005f8bd00106115034879015400e4b8cf396ae6
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2022-04-22 07:54:48 +0200

    Add dn42-alias

--- .config/zsh/50-aliases.zsh
+++ .config/zsh/50-aliases.zsh
@@ -33,3 +33,6 @@ alias nixup='nix-channel --update && nix-env -iA nixpkgs.myPackages && nix-env -
 alias ssh-until='ssh -o ConnectTimeout=10 -o ConnectionAttempts=360'
 
 alias zsh-benchmark-hyperfine="hyperfine -L userconfig true,false 'ZSH_NO_USER_CONFIG={userconfig} zsh -i -c exit'"
+
+
+alias dn42-bgp-status='ssh dn42-node sudo birdc show protocols | grep BGP | column -t'
