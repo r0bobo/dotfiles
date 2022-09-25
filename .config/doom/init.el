f8cfc454bc1e0195d2fb1b749f8d5335a213eb1d
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2022-09-25 21:23:55 +0200

    Disable dirvish

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -67,7 +67,7 @@
        word-wrap           ; soft wrapping with language-aware indent
 
        :emacs
-       (dired +dirvish)  ; making dired pretty [functional]
+       dired             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        ;;ibuffer         ; interactive buffer management
        (undo +tree)      ; persistent, smarter undo for your inevitable mistakes
