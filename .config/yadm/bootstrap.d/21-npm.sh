e39c50d1768ece80f61ee6bce5c5658e1b76cf50
Dean Lindqvist Todevski <dean.todevski@relexsolutions.com>
2023-11-08 06:35:47 +0100

    Update global npm packages install script

--- .config/yadm/bootstrap.d/21-npm.sh
+++ .config/yadm/bootstrap.d/21-npm.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-npm -g install \
-	prettier \
-	stylelint
+npminstall() {
+    npm install --global $*
+}
+
+npminstall prettier@3.0.3
+npminstall stylelint@15.11.0
