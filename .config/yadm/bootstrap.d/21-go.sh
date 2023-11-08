74c6d123cf7c9982ed27d03e85fb958d628c3f5c
Dean Lindqvist Todevski <dean.todevski@relexsolutions.com>
2023-11-08 05:32:17 +0100

    Bump gopls to 0.14.1

--- .config/yadm/bootstrap.d/21-go.sh
+++ .config/yadm/bootstrap.d/21-go.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-go install github.com/fatih/gomodifytags@v1.16.0
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.53.1
-go install golang.org/x/tools/cmd/godoc@v0.9.3
-go install golang.org/x/tools/cmd/goimports@v0.9.3
-go install golang.org/x/tools/cmd/gorename@v0.9.3
-go install golang.org/x/tools/cmd/guru@v0.9.3
-go install golang.org/x/tools/gopls@v0.12.2
-go install github.com/rogpeppe/godef@v1.1.2
+goinstall() {
+    echo Installing go package $* >&2
+    go install $*
+}
 
-go install github.com/google/go-containerregistry/cmd/crane@v0.15.2
+goinstall github.com/fatih/gomodifytags@v1.16.0
+goinstall github.com/golangci/golangci-lint/cmd/golangci-lint@v1.53.1
+goinstall golang.org/x/tools/cmd/godoc@v0.9.3
+goinstall golang.org/x/tools/cmd/goimports@v0.9.3
+goinstall golang.org/x/tools/cmd/gorename@v0.9.3
+goinstall golang.org/x/tools/cmd/guru@v0.9.3
+goinstall golang.org/x/tools/gopls@v0.14.1
+goinstall github.com/rogpeppe/godef@v1.1.2
 
-go install rsc.io/2fa@v1.2.0
+goinstall github.com/google/go-containerregistry/cmd/crane@v0.15.2
+
+goinstall rsc.io/2fa@v1.2.0
