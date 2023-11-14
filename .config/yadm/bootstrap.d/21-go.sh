70175ee300c87f836b48b760cc50c96e859b78ba
Dean Lindqvist Todevski <dean.todevski@relexsolutions.com>
2023-11-14 21:34:11 +0100

    Update go bootstrap files

--- .config/yadm/bootstrap.d/21-go.sh
+++ .config/yadm/bootstrap.d/21-go.sh
@@ -6,15 +6,14 @@ goinstall() {
     go install $*
 }
 
+goinstall github.com/bazelbuild/bazelisk@v1.18.0
 goinstall github.com/fatih/gomodifytags@v1.16.0
 goinstall github.com/golangci/golangci-lint/cmd/golangci-lint@v1.53.1
+goinstall github.com/google/go-containerregistry/cmd/crane@v0.15.2
+goinstall github.com/rogpeppe/godef@v1.1.2
 goinstall golang.org/x/tools/cmd/godoc@v0.9.3
 goinstall golang.org/x/tools/cmd/goimports@v0.9.3
 goinstall golang.org/x/tools/cmd/gorename@v0.9.3
 goinstall golang.org/x/tools/cmd/guru@v0.9.3
 goinstall golang.org/x/tools/gopls@v0.14.1
-goinstall github.com/rogpeppe/godef@v1.1.2
-
-goinstall github.com/google/go-containerregistry/cmd/crane@v0.15.2
-
 goinstall rsc.io/2fa@v1.2.0
