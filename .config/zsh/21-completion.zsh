522b8df3ac82eefd6ce8d7623474f91a592e4da5
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2023-11-24 23:29:00 +0100

    Add kind completion

--- .config/zsh/21-completion.zsh
+++ .config/zsh/21-completion.zsh
@@ -4,10 +4,11 @@ autoload -Uz compinit && compinit
 autoload -U +X bashcompinit && bashcompinit
 
 # Zsh completion
-dean::zshcomp helm completion zsh
-dean::zshcomp kubectl completion zsh
-dean::zshcomp doom make completions --zsh
 dean::zshcomp crane completion zsh
+dean::zshcomp doom make completions --zsh
+dean::zshcomp helm completion zsh
+dean::zshcomp kind completion zsh
+dean::zshcomp kubectl completion zsh
 
 # Bash completion
 dean::bashcomp aws aws_completer
