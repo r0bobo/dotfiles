06687fc0256225c08eb1f17b04b9df6a671cd1c3
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2024-01-01 13:04:36 +0100

    Handle yadm magit without tramp

--- .config/doom/modules/tools/yadm/autoload.el
+++ .config/doom/modules/tools/yadm/autoload.el
@@ -7,12 +7,23 @@
     (split-string
      (shell-command-to-string "yadm list"))))
 
-; Inspired by https://philjackson.github.io//yadm/emacs/magit/2021/07/25/using-yadm-via-magit/
+;;; https://github.com/magit/magit/issues/460#issuecomment-1475082958
 ;;;###autoload
-(defun +yadm/magit-status ()
-  "Show Magit status for yadm repo."
-  (interactive)
-  (magit-status "/yadm::"))
+(defun +yadm/magit-process-environment (env)
+  "Detect and set git -bare repo env vars when in tracked dotfile directories."
+  (let* ((default (file-name-as-directory (expand-file-name default-directory)))
+         (git-dir (expand-file-name "~/.local/share/yadm/repo.git/"))
+         (work-tree (expand-file-name "~/"))
+         (dotfile-dirs
+          (-map (apply-partially 'concat work-tree)
+                (-uniq (-keep #'file-name-directory (split-string (shell-command-to-string
+                                                                   (format "/usr/bin/git --git-dir=%s --work-tree=%s ls-tree --full-tree --name-only -r HEAD"
+                                                                           git-dir work-tree))))))))
+    (push work-tree dotfile-dirs)
+    (when (member default dotfile-dirs)
+      (push (format "GIT_WORK_TREE=%s" work-tree) env)
+      (push (format "GIT_DIR=%s" git-dir) env)))
+  env)
 
 ;;;###autoload
 (defun +yadm/find-file ()
--- .config/doom/modules/tools/yadm/config.el
+++ .config/doom/modules/tools/yadm/config.el
@@ -1,17 +1,11 @@
 ;;; tools/yadm/config.el -*- lexical-binding: t; -*-
 
-(after! tramp
-  (add-to-list 'tramp-methods
-               '("yadm"
-                 (tramp-login-program "yadm")
-                 (tramp-login-args (("enter")))
-                 (tramp-login-env (("SHELL") ("/bin/sh")))
-                 (tramp-remote-shell "/bin/sh")
-                 (tramp-remote-shell-args ("-c")))))
+(after! magit
+  (advice-add 'magit-process-environment
+              :filter-return #'+yadm/magit-process-environment))
 
 (map!
  :leader
  :prefix ("y" . "yadm")
  :desc "Add file to yadm" "a" #'+yadm/add-file
- :desc "Find yadm managed file" "f" #'+yadm/find-file
- :desc "Yadm Magit status" "g" #'+yadm/magit-status)
+ :desc "Find yadm managed file" "f" #'+yadm/find-file)
