755a976439878e439f3a9b4f478ff525939e5529
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2024-01-04 12:45:07 +0100

    Update yadm code

--- .config/doom/modules/tools/yadm/autoload.el
+++ .config/doom/modules/tools/yadm/autoload.el
@@ -7,22 +7,27 @@
     (split-string
      (shell-command-to-string "yadm list"))))
 
+;;;###autoload
+(defun +yadm--tracked-file-p ()
+  "Return t if current buffer is file tracked by yadm."
+  (unless (vc-call-backend 'Git 'root default-directory)
+    (let* ((default (file-name-as-directory (expand-file-name default-directory)))
+           (dotfile-dirs
+            (-map (apply-partially 'concat +yadm-work-tree)
+                  (-uniq (-keep #'file-name-directory (split-string (shell-command-to-string
+                                                                     (format "/usr/bin/git --git-dir=%s --work-tree=%s ls-tree --full-tree --name-only -r HEAD"
+                                                                             +yadm-git-dir +yadm-work-tree))))))))
+      (push +yadm-work-tree dotfile-dirs)
+      (when (member default dotfile-dirs)
+        t))))
+
 ;;; https://github.com/magit/magit/issues/460#issuecomment-1475082958
 ;;;###autoload
 (defun +yadm/magit-process-environment (env)
-  "Detect and set git -bare repo env vars when in tracked dotfile directories."
-  (let* ((default (file-name-as-directory (expand-file-name default-directory)))
-         (git-dir (expand-file-name "~/.local/share/yadm/repo.git/"))
-         (work-tree (expand-file-name "~/"))
-         (dotfile-dirs
-          (-map (apply-partially 'concat work-tree)
-                (-uniq (-keep #'file-name-directory (split-string (shell-command-to-string
-                                                                   (format "/usr/bin/git --git-dir=%s --work-tree=%s ls-tree --full-tree --name-only -r HEAD"
-                                                                           git-dir work-tree))))))))
-    (push work-tree dotfile-dirs)
-    (when (member default dotfile-dirs)
-      (push (format "GIT_WORK_TREE=%s" work-tree) env)
-      (push (format "GIT_DIR=%s" git-dir) env)))
+  "Add yadm GIT_DIR and GIT_WORK_TREE to magit env if current file is tracked by yadm."
+  (when (+yadm--tracked-file-p)
+    (push (concat "GIT_WORK_TREE=" +yadm-work-tree) env)
+    (push (concat "GIT_DIR=" +yadm-git-dir) env))
   env)
 
 ;;;###autoload
--- .config/doom/modules/tools/yadm/config.el
+++ .config/doom/modules/tools/yadm/config.el
@@ -1,6 +1,14 @@
 ;;; tools/yadm/config.el -*- lexical-binding: t; -*-
 
+(defconst +yadm-git-dir (expand-file-name "~/.local/share/yadm/repo.git/")
+  "YADM GIT_DIR path.")
+
+(defconst +yadm-work-tree (expand-file-name "~/")
+  "YADM GIT_WORK_TREE path.")
+
 (after! magit
+  ;; Inspired by:
+  ;; https://github.com/magit/magit/issues/460#issuecomment-1475082958
   (advice-add 'magit-process-environment
               :filter-return #'+yadm/magit-process-environment))
 
