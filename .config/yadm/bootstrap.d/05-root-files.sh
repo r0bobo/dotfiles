db11a2d1f92ca826e4fa23ada6e6fd3cb477defe
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2024-01-08 15:52:15 +0100

    Add missing yadm bootstrap script

---
+++ .config/yadm/bootstrap.d/05-root-files.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+root_files_path="$BOOTSTRAP_DIR/root-files"
+
+shopt -s globstar
+
+for src in "$root_files_path"/**; do
+    [[ -d "$src" ]] && continue
+    dest="/$(realpath --relative-to="$root_files_path" "$src")"
+
+    sudo install -CDvm0644 "$src" "$dest"
+done
