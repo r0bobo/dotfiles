4cde24fceaaeb1065d356cdb33d3121a590e35d6
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2024-02-09 06:06:05 +0100

    Update go-tools to v0.17.0 (#25)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- .config/yadm/bootstrap.d/21-go.sh
+++ .config/yadm/bootstrap.d/21-go.sh
@@ -17,9 +17,9 @@ goinstall github.com/rogpeppe/godef@v1.1.2
 goinstall github.com/warrensbox/terraform-switcher@0.13.1308
 goinstall github.com/warrensbox/tgswitch@0.6.0
 goinstall mvdan.cc/gofumpt@v0.6.0
-goinstall golang.org/x/tools/cmd/godoc@v0.16.1
-goinstall golang.org/x/tools/cmd/goimports@v0.16.1
-goinstall golang.org/x/tools/cmd/gorename@v0.16.1
-goinstall golang.org/x/tools/cmd/guru@v0.16.1
+goinstall golang.org/x/tools/cmd/godoc@v0.17.0
+goinstall golang.org/x/tools/cmd/goimports@v0.17.0
+goinstall golang.org/x/tools/cmd/gorename@v0.17.0
+goinstall golang.org/x/tools/cmd/guru@v0.17.0
 goinstall golang.org/x/tools/gopls@v0.14.2
 goinstall rsc.io/2fa@v1.2.0
