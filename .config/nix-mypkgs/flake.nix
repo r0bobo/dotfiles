eafb81f0b181cbc8de40d77d84afe0e7dd7a05a1
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2024-02-26 07:31:01 +0100

    Move more packages to nix

--- .config/nix-mypkgs/flake.nix
+++ .config/nix-mypkgs/flake.nix
@@ -8,14 +8,24 @@
       in pkgs.buildEnv {
         name = "home-packages";
         paths = with pkgs; [
+          bazelisk
+          crane
           devbox
           dive
           elixir_ls
           emacs-lsp-booster
           exercism
+          go-2fa
+          godef
+          gofumpt
+          golangci-lint
+          gomodifytags
+          gopls
+          gotools
           grex
           hey
           istioctl
+          javascript-typescript-langserver
           k9s
           kail
           kind
@@ -23,10 +33,22 @@
           kubernetes-helm
           minikube
           nix-zsh-completions
+          nodePackages.prettier
+          nodePackages.stylelint
           rbw
           rustup
           solargraph
           starship
+          tf-summarize
+          tfautomv
+          tflint
+          tflint-plugins.tflint-ruleset-google
+          tfswitch
+          tgswitch
+          tmuxPlugins.extrakto
+          typescript
+          vscode-langservers-extracted
+          yadm
           yaml-language-server
           yq-go
         ];
--- .config/yadm/bootstrap.d/21-go.sh
+++ .config/yadm/bootstrap.d/21-go.sh
@@ -6,20 +6,4 @@ goinstall() {
     go install "$@"
 }
 
-goinstall github.com/bazelbuild/bazelisk@v1.19.0
-goinstall github.com/busser/tfautomv@v0.6.0
-goinstall github.com/fatih/gomodifytags@v1.16.0
-goinstall github.com/golangci/golangci-lint/cmd/golangci-lint@v1.56.2
-goinstall github.com/google/go-containerregistry/cmd/crane@v0.19.0
-goinstall github.com/google/ko@v0.15.2
 goinstall github.com/psanford/tpm-fido@5f8828b82b58f9badeed65718fca72bc31358c5c
-goinstall github.com/rogpeppe/godef@v1.1.2
-goinstall github.com/warrensbox/terraform-switcher@0.13.1308
-goinstall github.com/warrensbox/tgswitch@0.6.0
-goinstall mvdan.cc/gofumpt@v0.6.0
-goinstall golang.org/x/tools/cmd/godoc@v0.18.0
-goinstall golang.org/x/tools/cmd/goimports@v0.18.0
-goinstall golang.org/x/tools/cmd/gorename@v0.18.0
-goinstall golang.org/x/tools/cmd/guru@v0.18.0
-goinstall golang.org/x/tools/gopls@v0.14.2
-goinstall rsc.io/2fa@v1.2.0
