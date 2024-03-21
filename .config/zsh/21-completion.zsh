510dd04f8d4d69ed679f6333f383833c7cf621f5
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2024-03-21 07:35:06 +0100

    Remove obsolete shell completion config

--- .config/zsh/21-completion.zsh
+++ .config/zsh/21-completion.zsh
@@ -4,11 +4,7 @@ autoload -Uz compinit && compinit
 autoload -U +X bashcompinit && bashcompinit
 
 # Zsh completion
-dean::zshcomp crane completion zsh
 dean::zshcomp doom make completions --zsh
-dean::zshcomp helm completion zsh
-dean::zshcomp kind completion zsh
-dean::zshcomp kubectl completion zsh
 
 # Bash completion
 dean::bashcomp aws aws_completer
