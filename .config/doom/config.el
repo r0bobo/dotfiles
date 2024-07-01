c1ebcc678fb575edaf0ac6e02702ae77cc80045f
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2024-07-01 21:19:14 +0200

    Remove obsolete casual package config

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -267,11 +267,6 @@ the `projectile-default-project-name' function is used."
 (use-package! consult-gh
   :after consult)
 
-(use-package! casual
-  :config
-  (map! :map calc-mode-map
-        "C-o" 'casual-main-menu))
-
 (use-package! casual-dired
   :config
   (map! :map dired-mode-map
