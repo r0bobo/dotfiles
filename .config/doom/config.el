83a2f052baf04f3de3ca25b10abc47ed88354663
Dean Lindqvist Todevski <dean.todevski@gmail.com>
2025-01-14 07:54:59 +0100

    Enable new features in starpls

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -316,7 +316,9 @@
       :new-connection (lsp-stdio-connection
                        (lambda ()
                          (list "starpls" "server"
+                               "--experimental_enable_label_completions"
                                "--experimental_infer_ctx_attributes"
+                               "--experimental_use_code_flow_analysis"
                                (concat "--bazel_path=" (executable-find "bazelisk")))))
       :activation-fn (lsp-activate-on "bazel")
       :environment-fn (lambda ()
